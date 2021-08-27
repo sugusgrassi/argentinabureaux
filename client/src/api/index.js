f17e620a224cc2a2571687f1e0d86362154fc573
sugus.grassi <sugus.grassi@gmail.com>
2021-08-27 19:42:33 -0300

    new api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const url = 'https://argentinabureaux.app.com/posts';
+const url = 'https://argentina-bureaux.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
